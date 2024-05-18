d7b9d938d3c52a97279fc3487b16464bb6b709dd
TitasRoy14 <roytitas800@gmail.com>
2024-05-18 11:15:48 +0530

    Show toast notifications

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,5 +3,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({ orderBy: { name: 'asc' } });
-  return NextResponse.json(users);
+  return NextResponse.json(users, { status: 201 });
+}
+
+export async function POST(request: NextRequest) {
+  const userOb = await request.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userOb.userId,
+    },
+  });
+
+  return NextResponse.json(user, { status: 200 });
 }
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,6 +3,7 @@ import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 import Skeleton from '@/app/components/Skeleton';
 
@@ -25,29 +26,63 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 
   if (error) return null;
 
+  const assignToUser = (userId: string) => {
+    return axios.patch<Issue>('/api/issues/' + issue.id, {
+      assignedToUserId: userId === 'none' ? null : userId,
+    });
+  };
+
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || 'none'}
-      onValueChange={(userId) => {
-        axios.patch('/api/issues/' + issue.id, {
-          assignedToUserId: userId === 'none' ? null : userId,
-        });
-      }}
-    >
-      <Select.Trigger placeholder='Assign' />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value='none'>Unassigned</Select.Item>
-          {users?.map((usr) => (
-            <Select.Item key={usr.id} value={usr.id}>
-              {usr.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || 'none'}
+        onValueChange={async (userId) => {
+          let userName: string;
+
+          if (userId !== 'none') {
+            const { name } = await axios
+              .post<User>('/api/users', { userId: userId || null })
+              .then((res) => res.data);
+            userName = name!;
+          }
+
+          toast.promise(assignToUser(userId), {
+            loading: 'Loading...',
+            success: (data) => {
+              return userId === 'none'
+                ? `Issue reverted to ${'Unassigned'}`
+                : `Issue assigned to ${userName}`;
+            },
+
+            error: (err: Error) => `This just happened: ${console.log(err)}`,
+          });
+        }}
+      >
+        <Select.Trigger placeholder='Assign' />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value='none'>Unassigned</Select.Item>
+            {users?.map((usr) => (
+              <Select.Item key={usr.id} value={usr.id}>
+                {usr.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
+
+// async function fetchUser(userId: string) {
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       id: userId,
+//     },
+//   });
+//   return user;
+// }
 
 export default AssigneeSelect;
